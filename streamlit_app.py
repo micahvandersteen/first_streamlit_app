1d4ca7f5aed6cef6c90773d8f67d3d88609ebb38
Micah Vandersteen <micahvandersteen@gmail.com>
2023-06-27 13:33:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
+# Title 
 streamlit.title('My Parents New Healthy Diner')
+
+# Header
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
