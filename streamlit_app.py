3fed812b899b8b4a05927409e432d00820a53d20
Micah Vandersteen <micahvandersteen@gmail.com>
2023-06-27 13:40:18 -0600

    adding items

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,14 @@ streamlit.title('My Parents New Healthy Diner')
 
 # Header
 streamlit.header('Breakfast Favorites')
+
+# Breakfast favorites Items
 streamlit.text('🫐 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🥑 🍞 Avocado Toast')
+
+# BYO Smoothie Header
+streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie')
 
 
